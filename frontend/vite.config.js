9cf4d1094c07ad852f682291003894f6eacd04a1
Jose María <jmcamposdev@gmail.com>
2024-01-31 09:36:11 +0000

    Agregada configuración de ruta base en vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Esto debería establecer la base de la ruta relativa
   plugins: [react()],
   server: {
     proxy: {
